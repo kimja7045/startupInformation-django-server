9bb5cf3739a1b8870faf42c99d6995984231a0d0
kimja01 <kimja7045@naver.com>
2020-09-29 18:24:47 +0900

    feat(admin): Add admin filter

--- service/admin/user.py
+++ service/admin/user.py
@@ -1,11 +1,13 @@
 from django.contrib.gis import admin
 from django.contrib.auth.admin import UserAdmin
 
-from service.models.user import *
+from service.models import *
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('phone_number', 'profile_image',)}),
+        (None, {'fields': ('nickname', 'phone_number')}),
     )
+    list_display = ('nickname', 'phone_number', 'email')
+    search_fields = ('nickname', 'phone_number', 'email')
