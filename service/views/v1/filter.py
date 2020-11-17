d3d2b61fde8832c4603e6d2036af8981425d3dd8
kimja01 <kimja7045@naver.com>
2020-11-18 01:53:43 +0900

    Update 찜 유저 필터

--- service/views/v1/filter.py
+++ service/views/v1/filter.py
@@ -7,14 +7,22 @@ import django_filters
 
 class PostFilter(filters.FilterSet):
     keyword = CharFilter(method='get_keyword')
+    favorite_user = CharFilter(method='get_favorite_user')
 
     class Meta:
         model = Post
         fields = (
             'keyword',
+            'user',
+            'favorite_user',
         )
 
     def get_keyword(self, queryset, name, value):
         return queryset.filter(
             title__contains=value
         ).distinct()
+
+    def get_favorite_user(self, queryset, name, value):
+        return queryset.filter(
+            favorite_users=value,
+        )
